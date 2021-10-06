8009ae493656adf48f5e5bf43699aed432761821
belikor <belikor@users.noreply.github.com>
2021-10-06 13:41:43 -0500

    dseed: use ttk widgets where available for better appearance
    Use `'clam'` theme in Linux.
    
    Also add a monospace font for the entry widgets.

--- dseed.py
+++ dseed.py
@@ -25,24 +25,28 @@
 # --------------------------------------------------------------------------- #
 """Small application to download claims from LBRY channels."""
 import os
+import platform
 import sys
 import tkinter as tk
+import tkinter.font
+import tkinter.ttk as ttk
 
 import lbseed.res_download as rs
 import lbseed.validate as vd
 
 
-class Application(tk.Frame):
+class Application(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
         self.setup_widgets(parent)
 
     def setup_widgets(self, parent):
+        self.ftmono = tk.font.Font(family="monospace", size=10)
         self.setup_top(parent)
         self.setup_textbox(parent)
 
     def setup_top(self, parent):
-        frame = tk.Frame(parent)
+        frame = ttk.Frame(parent)
         frame.pack(padx=4, pady=4)
         self.setup_grid_top(frame, start=0)
         self.setup_grid_low(frame, start=2)
@@ -52,81 +56,83 @@ class Application(tk.Frame):
         self.server_var = tk.StringVar()
         self.server_var.set("http://localhost:5279")
 
-        entry = tk.Entry(parent, textvariable=self.server_var)
+        entry = ttk.Entry(parent, textvariable=self.server_var,
+                          font=self.ftmono)
         entry.grid(row=start, column=0, sticky=tk.W + tk.E)
-        le = tk.Label(parent,
-                      text=("Address of the 'lbrynet' daemon. "
-                            "It defaults to localhost:5279"))
+        le = ttk.Label(parent,
+                       text=("Address of the 'lbrynet' daemon. "
+                             "It defaults to localhost:5279"))
         le.grid(row=start, column=1, sticky=tk.W, padx=2)
 
         self.down_dir_var = tk.StringVar()
         self.down_dir_var.set(rs.get_download_dir(server=self.server_var.get()))
 
-        entry_dir = tk.Entry(parent, textvariable=self.down_dir_var)
+        entry_dir = ttk.Entry(parent, textvariable=self.down_dir_var,
+                              font=self.ftmono)
         entry_dir.grid(row=start+1, column=0, sticky=tk.W + tk.E)
-        ledir = tk.Label(parent,
-                         text=("Download directory. "
-                               "It defaults to your home directory."))
+        ledir = ttk.Label(parent,
+                          text=("Download directory. "
+                                "It defaults to your home directory."))
         ledir.grid(row=start+1, column=1, sticky=tk.W, padx=2)
 
     def setup_grid_low(self, parent, start=0):
-        _width = 20
-        b_validate = tk.Button(parent, text="Validate input",
-                               width=_width,
-                               command=self.validate)
+        _width = 26
+        b_validate = ttk.Button(parent, text="Validate input",
+                                width=_width,
+                                command=self.validate)
         b_validate.grid(row=start, column=0)
         b_validate.focus()
-        lv = tk.Label(parent,
-                      text="Verify that the input can be read correctly")
+        lv = ttk.Label(parent,
+                       text="Verify that the input can be read correctly")
         lv.grid(row=start, column=1, sticky=tk.W, padx=2)
 
-        b_resolve = tk.Button(parent, text="Resolve online",
-                              width=_width,
-                              command=self.resolve)
+        b_resolve = ttk.Button(parent, text="Resolve online",
+                               width=_width,
+                               command=self.resolve)
         b_resolve.grid(row=start+1, column=0)
-        lr = tk.Label(parent,
-                      text="Confirm that the channels exist")
+        lr = ttk.Label(parent,
+                       text="Confirm that the channels exist")
         lr.grid(row=start+1, column=1, sticky=tk.W, padx=2)
 
-        b_download = tk.Button(parent, text="Download claims",
-                               width=_width,
-                               command=self.start_download)
+        b_download = ttk.Button(parent, text="Download claims",
+                                width=_width,
+                                command=self.start_download)
         b_download.grid(row=start+2, column=0)
-        lr = tk.Label(parent,
-                      text=("Start downloading the newest claims "
-                            "from the channels"))
+        lr = ttk.Label(parent,
+                       text=("Start downloading the newest claims "
+                             "from the channels"))
         lr.grid(row=start+2, column=1, sticky=tk.W, padx=2)
 
         self.own_dir_var = tk.BooleanVar()
         self.own_dir_var.set(True)
-        chk_owndir = tk.Checkbutton(parent,
-                                    variable=self.own_dir_var)
+        chk_owndir = ttk.Checkbutton(parent,
+                                     variable=self.own_dir_var)
         chk_owndir.grid(row=start+3, column=0, sticky=tk.E)
-        lchk = tk.Label(parent,
-                        text="Place the downloaded file inside a subdirectory")
+        lchk = ttk.Label(parent,
+                         text="Place the downloaded file inside a subdirectory")
         lchk.grid(row=start+3, column=1, sticky=tk.W, padx=2)
 
         self.save_var = tk.BooleanVar()
         self.save_var.set(True)
-        chk_save = tk.Checkbutton(parent,
-                                  variable=self.save_var)
+        chk_save = ttk.Checkbutton(parent,
+                                   variable=self.save_var)
         chk_save.grid(row=start+4, column=0, sticky=tk.E)
-        lchk_save = tk.Label(parent,
-                             text=("Save media and its blobs; "
-                                   "otherwise only the first blob"))
+        lchk_save = ttk.Label(parent,
+                              text=("Save media and its blobs; "
+                                    "otherwise only the first blob"))
         lchk_save.grid(row=start+4, column=1, sticky=tk.W, padx=2)
 
     def setup_info(self, parent, start):
-        info = tk.Label(parent,
-                        text=("Add a channel, a comma, "
-                              "and the number of items to download "
-                              "from this channel"))
+        info = ttk.Label(parent,
+                         text=("Add a channel, a comma, "
+                               "and the number of items to download "
+                               "from this channel"))
         info.grid(row=start, column=0, columnspan=2, sticky=tk.W)
 
     def setup_textbox(self, parent):
-        hsrl = tk.Scrollbar(parent, orient="horizontal")
+        hsrl = ttk.Scrollbar(parent, orient="horizontal")
         hsrl.pack(side=tk.BOTTOM, fill=tk.X)
-        vsrl = tk.Scrollbar(parent)
+        vsrl = ttk.Scrollbar(parent)
         vsrl.pack(side=tk.RIGHT, fill=tk.Y)
 
         self.textbox = tk.Text(parent,
@@ -193,6 +199,11 @@ class Application(tk.Frame):
 def main(argv=None):
     root = tk.Tk()
     app = Application(root)
+
+    them = ttk.Style()
+    if "linux" in platform.system().lower():
+        them.theme_use("clam")
+
     app.master.title("lbrydseed")
     app.mainloop()
 
